0adc436db43e7c4580df9efce039899cb9fe2a6f
kimoleg_workstation <kimoleg200301@gmail.com>
2025-01-28 13:24:26 +0500

    Minor changes

--- src/main/java/org/example/edujournalbackend/controller/StudentsController.java
+++ src/main/java/org/example/edujournalbackend/controller/StudentsController.java
@@ -1,7 +1,14 @@
 package org.example.edujournalbackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
+@RestController
+@RequestMapping("/api")
 public class StudentsController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
