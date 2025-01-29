61d1f39e3f9e3d16ca8127aa6e4a65172d802339
kimoleg_workstation <kimoleg200301@gmail.com>
2025-01-29 11:22:34 +0500

    added sql scripts

---
+++ sql/Script-24.sql
@@ -0,0 +1,37 @@
+create table students (
+	student_id int primary key,
+	firstname varchar(100) not null,
+	lastname varchar(100) not null,
+	birth_date date not null,
+	gender varchar(100) not null,
+	IIN varchar(12),
+	living_adress varchar(150),
+	edu_group_id int
+);
+create table edu_groups (
+	edu_group_id int primary key,
+	name varchar(150) not null,
+	created date not null
+); /* the main one from tables list_of_subjects, subjects and journals */
+create table subjects (
+	subject_id int primary key,
+	name varchar(150) not null,
+	subject_code varchar(100) not null,
+	credits int not null
+); /* <-- journals */
+create table list_of_subjects (
+	list_of_subjects_id int primary key,
+	edu_group_id int not null,
+	subject_id int not null,
+	foreign key(edu_group_id) references edu_groups(edu_group_id)
+);
+create table journals (
+	journal_id int primary key,
+	subject_id int,
+	student_id int not null,
+	mark int check(mark between 1 and 100),
+	foreign key (student_id) references students(student_id),
+	foreign key(subject_id) references subjects(subject_id)
+);
+
+select * from spring_session_attributes ssa ;
